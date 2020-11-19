067a341b0e1b09f74a6ea0527108167333ca0259
Adam Figueroa <admiralfiggins@gmail.com>
2020-11-19 07:08:08 -0800

    final version

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const store = {
   // 5 or more questions are required
   questions: [
     {
-      question: 'What position does Harry Potter play in Quiddich??',
+      question: 'What position does Harry Potter play in Quiddich?',
       answers: [
         'Chaser',
         'Keeper',
@@ -32,10 +32,10 @@ const store = {
         'He is an Animagus.',
         'He is a Werewolf.'
       ],
-      correctAnswer: 'What does Felix Felicis potion do?'
+      correctAnswer: 'He is a Parselmouth.'
     },
     {
-      question: 'What is the current year?',
+      question: 'What does Felix Felicis potion do?',
       answers: [
         'It allows the drinker to assume the appearance of someone else.',
         'It is a deadly poison.',
@@ -67,14 +67,11 @@ const store = {
   ],
   quizStarted: false,
   questionNumber: 0,
-  score: 0
+  score: 0,
+  wrong: 0
 };
 
-{/* <div class="frontpage">
-    <h2>
-      Hey there! Welcome to my Harry Potter quiz! Are you a real fan? Well it's time to find out!
-    </h2>
-</div> */}
+
 /**
  * 
  * Technical requirements:
@@ -93,10 +90,55 @@ const store = {
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 
 // These functions return HTML templates
+function generateMainPage() {
+  return `<div class="mainPage">
+  <h2 class="quizTitle">Step right up to the Harry Potter Quiz!</h2>
+  <p class="introPage">Hey there! Welcome to my Harry Potter quiz! Are you a real fan? Well it's time to find out!</p>
+  <button id="startBtn">Start Quiz!</button>
+</div>
+`
+}
+
+function generateQuestionPage() {
+  let question = store.questions[store.questionNumber];
+  
+  let answers = question.answers.map((answer, index) => {
+    if (index === 0) {
+      return `<input type="radio" name="answer" id="answer${index}" class="answers" value="${answer}" required>
+      <label for="answer${index}">${answer}</label><br>`
+     } 
+      return `<input type="radio" name="answer" id="answer${index}" class="answers" value="${answer}">
+      <label for="answer${index}">${answer}</label><br>`
+  });
+  return `<div class="questionPage">
+  <span class="questionCounter">Question ${store.questionNumber + 1}/6</span>
+  <form id="questionForm">
+  <h2 class="question">${question.question}</h2>
+  ${answers.join("")}
+  <button id="answerBtn">Submit</button>
+  </form>
+  <span class="answerCounter">Correct: ${store.score} Incorrect: ${store.wrong}</span>
+</div>
+` 
+}
+
+function generateFinalPage() {
+  return `<div class="correctPage">
+  <h2 class="final">Yay! You finished the quiz!</h2>
+  <p class="answerCounter">Correct: ${store.score} Incorrect: ${store.wrong}</p>
+  <img class="correctGif" src="./images/party-hard.gif" alt="Harry and Ron smiling">
+  <button id="reset">Try Again?</button>
+</div>
+`
+}
+
+
 
 /********** RENDER FUNCTION(S) **********/
 
 // This function conditionally replaces the contents of the <main> tag based on the state of the store
+
+
 
 /********** EVENT HANDLER FUNCTIONS **********/
 
@@ -104,39 +146,92 @@ const store = {
 
 // function renderAll()
 // -render the main page
- 
+function renderPage() {
+  let html = ''
+  if (store.quizStarted) {
+    if (store.questionNumber === store.questions.length) {
+      html = generateFinalPage();
+    } else {
+      html = generateQuestionPage()
+    } 
+  } else {
+    html = generateMainPage()
+  }
+  $('main').html(html);
+}
+
+
 //  function handleStartBtn(){}
 // -Bind the event handler for startBtn 
 //   - the event handler for startBtn on click will load the quiz 
-// function quizPage(){}
-// -  rendering the quiz page 
-//   - dynamically generate main quiz html
-// function questionSection(){} 12-27 loop
-//   - generate question based on index :
-  
-//   function handleAnswer(){}
-//   - Bind the event handler for the check answer button : will function checkAnswer()
-   
+function handleStartQuizBtn() {
+  $('main').on('click', '#startBtn', function(event) {
+    store.quizStarted = true;
+    renderPage();
+  } )
+}
+
 //    function checkAnswer (){}
 //     - check if answer is correct 
-//       - display the check 
-//       -bind next button
-//       function handle updateQuestion(){}
-//     - update the question
-//      - update index : stored in STORE
-//      - update score : stored in STORE
-//     reached ned of the quiz ? display final : recall questionSection
-//      - display question/final
-// function renderFinal
-// - display final page  
-//  - display score 
+//       - display the check correct or incorrect
+//       - display final page  
+//       - display score 
+function checkAnswer() {
+  $('main').on('submit', '#questionForm', function(event) {
+    event.preventDefault();
+    let e = store.questions[store.questionNumber].correctAnswer;
+    let submitValue = $('input[name="answer"]:checked').val();
+    console.log(e, submitValue)
+     if (submitValue === e) {
+       store.score++;
+       $('main').html(`<div class="correctPage">
+       <h2 class="correct">Yay! you were correct!</h2>
+       <p class="answerCounter">Correct: ${store.score} Incorrect: ${store.wrong}</p>
+       <button id="nextBtn">Next Question</button>
+       <img class="correctGif" src="./images/harry-and-ron.gif" alt="Harry and Ron smiling">
+     </div>
+     `)
+     } else {
+       store.wrong++;
+       $('main').html(`<div class="incorrectPage">
+       <h2 class="incorrect">Oh no! That is the wrong answer!</h2>
+       <p class="answerCounter">Correct: ${store.score} Incorrect: ${store.wrong}</p>
+       <button id="nextBtn">Next Question</button>
+       <img class="incorrectGif" src="./images/harry-rubber-arm.gif" alt="Harry holding rubber arm in shock">
+     </div>
+     `)
+     }   
+  })
+}
+
+// - increment the questionNumber by 1
+// - render the page
+function handleNextBtn() {
+  $('main').on('click', '#nextBtn', function(event) {
+    store.questionNumber++;
+    renderPage();
+  })
+}
+
 //  - bind the resetBtn will call the quiz page function
-//   - restart the quiz 
+//   - restart the quiz
+function handleFinalBtn() {
+  $('main').on('click', '#reset', function(event) {
+    event.preventDefault();
+    store.quizStarted = false;
+    store.questionNumber = 0;
+    store.score = 0;
+    store.wrong = 0;
+    renderPage();
+  })
+}
 
 function main() {
-
-
-
+renderPage();
+handleStartQuizBtn()
+handleNextBtn()
+checkAnswer()
+handleFinalBtn()
 }
 
 $(main)
